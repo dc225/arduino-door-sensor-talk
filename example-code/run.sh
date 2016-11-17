5d73fc9843a67665611efa65ad9e76d92f787f70
i !== get('it') <josh.richard@gmail.com>
2016-11-16 20:02:49 -0600

    stahp that

--- example-code/run.sh
+++ example-code/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd ..; python ./serial.py >./python-output.log 2>&1 &
-cd ..; node ./monitor.js >./node-output.log 2>&1 &
+python ./serial.py >./python-output.log 2>&1 &
+node ./monitor.js >./node-output.log 2>&1 &
