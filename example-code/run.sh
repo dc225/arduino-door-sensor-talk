b0155cb141f4240b9ae9f36af9d5b91625495919
i !== get('it') <josh.richard@gmail.com>
2016-11-16 20:02:20 -0600

    Update run.sh

--- example-code/run.sh
+++ example-code/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python serial.py >./python-output.log 2>&1 &
-node monitor.js >./node-output.log 2>&1 &
+cd ..; python ./serial.py >./python-output.log 2>&1 &
+cd ..; node ./monitor.js >./node-output.log 2>&1 &
